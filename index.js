9b0187ba5f23a7d118c336215c4e364b9181232c
mobinmiah <mobinmiah1990@gmail.com>
2025-11-09 20:03:43 +0600

    starting the server seide of TravelEase

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+
