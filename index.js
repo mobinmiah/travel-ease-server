c171d7b6efd5031b3276df99e3793202eabb2da5
mobinmiah <mobinmiah1990@gmail.com>
2025-11-09 21:14:12 +0600

    starting the travel ease server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,3 +9,38 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// connection string
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9hcy35q.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+
+app.get('/', (req, res) => {
+    res.send('travelease is running')
+})
+
+async function run(){
+    try{
+        await client.connect();
+
+        const db = client.db("travel_ease")
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+    console.log(`travel ease server is running on port: ${port}`)
+})
