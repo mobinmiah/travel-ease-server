d16a4a5de2a4a2ce5ceb7548443e62d5914b51be
mobinmiah <mobinmiah1990@gmail.com>
2025-11-11 07:06:30 +0600

    created booking apis

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -32,12 +32,27 @@ async function run() {
         const db = client.db("travel_ease_db");
         const vehicles = db.collection("vehicles");
         const users = db.collection("users");
+        const bookings = db.collection("bookings");
 
         //    users apis
         app.get('/users', async (req, res) => {
             const cursor = users.find();
             const result = await cursor.toArray();
             res.send(result);
+        })
+
+        app.post('/users', async (req, res) => {
+            const newUser = req.body;
+            const email = req.body.email;
+            const query = { email: email };
+            const existingUser = await users.findOne(query)
+            if (existingUser) {
+                res.send({ message: 'User allready exist. no need add again' })
+            }
+            else {
+                const result = await users.insertOne(newUser);
+                res.send(result);
+            }
         })
 
         // vehicles apis
@@ -53,9 +68,32 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/vehicles/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await vehicles.findOne(query);
+            res.send(result);
+        })
 
+        app.post('/vehicles', async (req, res) => {
+            const newVehicle = req.body;
+            const result = await vehicles.insertOne(newVehicle);
+            res.send(result);
 
+        })
 
+        // bookings apis
+        app.get('/bookings', async (req, res) => {
+            const cursor = bookings.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const newBooking = req.body;
+            const result = await bookings.insertOne(newBooking);
+            res.send(result);
+        })
 
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
